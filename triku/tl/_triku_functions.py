9d8f1f76290f55a6a65e10e7eb06bcffc0e0245b
Alex M. Ascensión <alexmascension@gmail.com>
2019-12-20 13:22:09 +0100

    Return knee point done

--- triku/tl/_triku_functions.py
+++ triku/tl/_triku_functions.py
@@ -13,7 +13,7 @@ def find_starting_point(x, y, delta_y=None, delta_x=None):
         y_box = y[x_stop - delta_x: x_stop]
         y_diff = max(y_box) - min(y_box)
         if y_diff < delta_y:
-            return x_stop
+            return x_stop - delta_x
 
     return int(len(x) / 2)
 
@@ -31,7 +31,13 @@ def find_knee_point(x, y, s=0.0):
     This knee point, although not the most robust, is easy to calculate and interpret.
 
     The shortest distance between a c in C and R is (y_c - (m_r * x_c + b_r)) / (sqrt(1 + m_r ^ 2)). The knee point
-    is, therefore, the largest of the distances.
+    is, therefore, the largest of the distances. To obtain a more robust knee point, we will get first an intermediate
+    point which has a small slope.
+
+    After that we correct the knee_point selection usign the factor s. s is a factor that decreases the maximum distance
+    described before, selecting a point further left or right in the curve. A positive s will select a knee point
+    further right (higher mean - fewer points will be selected), and a negative s will select a knee point further left
+    (lower mean - more points will be selected).
     """
     x_0, x_f, y_0, y_f = x[0], x[-1], y[0], y[-1]
     m = (y_f - y_0) / (x_f - x_0)
@@ -40,7 +46,7 @@ def find_knee_point(x, y, s=0.0):
     list_d = [distance(m, b, x[i], y[i]) for i in range(len(x))]
 
     if len(list_d) < 3:
-        return 0, False
+        return 0
 
     if s >= 0:
         correction_factor = min(s, 0.9)
@@ -49,10 +55,11 @@ def find_knee_point(x, y, s=0.0):
         correction_factor = min(abs(s), 0.9)
         knee_x_idx = np.argwhere(list_d <= (1 - correction_factor) * min(list_d)).flatten()[0]
 
-    return knee_x_idx, True
+    return knee_x_idx
 
 
-def return_idx(prop_0: np.ndarray, mean: np.ndarray, percentile_low: float, percentile_high: float, s: float):
+def return_idx(prop_0: np.ndarray, mean: np.ndarray, percentile_low: float, percentile_high: float, s: float,
+               delta_x: int, delta_y: int):
     """
     Selection of the knee point in a curve. This function accepts the list of zero proportions, means, the
     cutting points, and the s parameter, and returns the indices from prop_0 whose mean is greater than the
@@ -68,18 +75,27 @@ def return_idx(prop_0: np.ndarray, mean: np.ndarray, percentile_low: float, perc
 
     x = np.arange(len(array_mean)) / len(array_mean)
     y = (np.sort(np.log10(array_mean)) - min(np.log10(array_mean))) / \
-        (np.max(np.log10(array_mean))  - np.min(np.log10(array_mean)))
+        (np.max(np.log10(array_mean)) - np.min(np.log10(array_mean)))
 
-    # Apply a Savitzky-Golay filter to remove noisiness on the curve. This filter will be applicable to arrays
-    # longer than 21 elements. This number is more or less arbitrary, but we have seen that for curves with fewer
-    # points it does introduce some artifacts.
+    '''
+    Apply a Savitzky-Golay filter to remove noisiness on the curve. This filter will be applicable to arrays
+    longer than 21 elements. This number is more or less arbitrary, but we have seen that for curves with fewer
+    points it does introduce some artifacts.
+    '''
     if len(y) > 21:
         y = savgol_filter(y, 2 * int(len(x) / 15) + 1, 3)
-    x_stop = find_starting_point(x, y)
-    knee_x_idx, status = find_knee_point(x[x_stop:], y[x_stop:], s)
 
-    if not status:  # There might not be a knee point because of the curve, or for any other reason
-        return []
+    '''
+    In some cases the curve does a small step, or falls and rises at the beginning, and these two cases destabilize
+    finding the knee point. To solve that problem we will "remove" those points from the curve, and then find the knee
+    point using the rest of the curve. 
+    
+    We find that point with the function `find_starting_point`. This function is a sliding window from right (where
+    the knee point is) to left, and, given a delta_x, creates a box of with delta_x and height y_f - y_0. If y_f - y_0
+    is smaller than delta_y, then it returns the left point of the box.
+    '''
+    x_stop = find_starting_point(x, y, delta_x=delta_x, delta_y=delta_y)
+    knee_x_idx, status = find_knee_point(x[x_stop:], y[x_stop:], s)
 
     knee_x_idx += x_stop
     knee_mean = np.sort(array_mean)[knee_x_idx]
@@ -90,7 +106,8 @@ def return_idx(prop_0: np.ndarray, mean: np.ndarray, percentile_low: float, perc
     return selector_idxs
 
 
-def return_triku_gene_idx(arr: np.ndarray, n_bins: int = 80, n_cycles: int = 4, s: float = 0):
+def return_triku_gene_idx(arr: np.ndarray, n_bins: int = 80, n_cycles: int = 4, s: float = 0, delta_x: int = None,
+                          delta_y: int = None):
     """
     Returns the indices of the array corresponding to the selected genes, with high variability.
     Parameters are explained in the main triku function.
@@ -121,8 +138,8 @@ def return_triku_gene_idx(arr: np.ndarray, n_bins: int = 80, n_cycles: int = 4, 
         for cycle in range(n_cycles):
             start_prop = prop_0_bins[N] + cycle * len_bin / n_cycles
             end_prop = prop_0_bins[N + 1] + cycle * len_bin / n_cycles
-            selector_idxs = return_idx(prop_0, mean, start_prop, end_prop, s)
-
+            selector_idxs = return_idx(prop_0, mean=mean, percentile_low=start_prop, percentile_high=end_prop, s=s,
+                                       delta_x=delta_x, delta_y=delta_y)
             selected_genes_index += selector_idxs
 
     # Remove duplicated entries
