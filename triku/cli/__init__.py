71d28478fba510b7b2c1f5f8942ba8d303864028
Alex M. Ascensión <alexmascension@gmail.com>
2019-12-31 11:43:42 +0100

    Added funcargparse

---
+++ triku/cli/__init__.py
@@ -0,0 +1,1 @@
+from .cli_triku import main
---
+++ triku/cli/cli_triku.py
@@ -0,0 +1,13 @@
+from triku.tl import triku
+from funcargparse import FuncArgParser
+
+def main():
+    parser = FuncArgParser()
+    parser.setup_args(triku)
+    parser.update_arg('object_triku', help="Path to the annData object or the count matrix.")
+    parser.create_arguments()
+    parser.parse2func()
+
+if __name__ == '__main__':
+    main()
+
