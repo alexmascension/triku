958f2dfd55feb14d47ba51d51fd712bf311db1b2
Alex M. Ascensión <alexmascension@gmail.com>
2021-08-11 17:27:50 +0200

    added sparsity tests

--- triku/test/test_units.py
+++ triku/test/test_units.py
@@ -1,8 +1,21 @@
 import numpy as np
 import pytest
 import scanpy as sc
+import scipy.sparse as spr
 
 import triku as tk
+
+
+@pytest.fixture
+def getpbmc3k():
+    adata = sc.datasets.pbmc3k()
+    sc.pp.filter_genes(adata, min_cells=10)
+    sc.pp.filter_cells(adata, min_genes=10)
+    sc.pp.neighbors(adata)
+
+    # tk.tl.triku(adata)
+
+    return adata
 
 
 @pytest.mark.exception_check
@@ -85,3 +98,20 @@ def test_triku_check_nonaccepted_type():
         pass
     else:
         raise BaseException
+
+
+@pytest.mark.exception_check
+def test_triku_dense_sparse_matrices(getpbmc3k):
+    adata_dense = getpbmc3k
+    adata_sparse = getpbmc3k.copy()
+
+    adata_dense.X = adata_dense.X.toarray()
+    adata_sparse.X = spr.csr.csr_matrix(adata_sparse.X)
+
+    tk.tl.triku(adata_sparse)
+    tk.tl.triku(adata_dense)
+
+    assert adata_dense.uns["triku_params"] == adata_sparse.uns["triku_params"]
+    assert np.all(
+        adata_dense.var["triku_distance"] == adata_sparse.var["triku_distance"]
+    )
